090c18496b5c0a011779da422f9152e078a81356
Johnson Dubula <jdubula@iqbusiness.net>
2021-10-15 23:41:23 +0200

    Functions

--- Function1.cs
+++ Function1.cs
@@ -17,17 +17,15 @@ namespace FunctionApp42
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            dynamic data = JsonConvert.DeserializeObject<User>(requestBody);
+            UserContext userContext = new UserContext();
+            userContext.Add(data);
+            userContext.SaveChanges();
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            string responseMessage = ".";
 
             return new OkObjectResult(responseMessage);
         }
